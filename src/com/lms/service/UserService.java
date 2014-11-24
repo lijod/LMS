52d98ab0e5729c8d834dbd078c186a940bf3aacc
vikas Sangwan <sangwan@ccs.neu.edu>
2014-11-23 21:21:49 -0500

    completed createUserService in com.lms.service

--- src/com/lms/service/UserService.java
+++ src/com/lms/service/UserService.java
@@ -1,6 +1,9 @@
 package com.lms.service;
 
+import java.util.Date;
+
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import com.lms.dao.UserDao;
 import com.lms.model.User;
@@ -8,19 +11,22 @@ import com.lms.model.User;
 // /com.lms.servic/JWSUserService
 @Path("/jwsUserService")
 public class UserService {
-	
+
 	// /com.lms.servic/JWSUserService/createUser
-	
+
 	@Path("/createUser/{userName}/{password}/{firstName}/{lastName}/{email}/{dateOfBirth}}")
-	public void createUserService(User user){
-	UserDao userObj = new UserDao();
-	userObj.createUser(user);
+	public User createUserService(@PathParam("userName") String userName,
+			@PathParam("password") String password,
+			@PathParam("firstName") String firstName,
+			@PathParam("lastName") String lastName,
+			@PathParam("email") String email,
+			@PathParam("dateOfBirth") Date dateOfBirth) {
+		User user =new User (userName,password,firstName,lastName,email,dateOfBirth);
+		UserDao userObj = new UserDao();
+		userObj.createUser(user);
+		return user;
 	}
-	
-	@Path("/findUserByUserId/{}")
-	public void findUserByUserIdService(User user){
-	UserDao userObj = new UserDao();
-	userObj.createUser(user);
-	}
+
+
 
 }
