60f99df979f05ff0ce883362a62bd9ebdb407e3a
lijod <lijod@ccs.neu.edu>
2014-12-03 18:10:30 -0500

    thread & post create

---
+++ MISC_FILES/post_thread_create_table.sql
@@ -0,0 +1,63 @@
+/*
+SQLyog Community v9.63 
+MySQL - 5.6.21-log : Database - lms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`lms` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `lms`;
+
+/*Table structure for table `post` */
+
+DROP TABLE IF EXISTS `post`;
+
+CREATE TABLE `post` (
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_content` blob NOT NULL,
+  `post_time` time NOT NULL,
+  `post_date` date NOT NULL,
+  `thread_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `post_user_fk2` (`user_id`),
+  KEY `thread_post_fk2` (`thread_id`),
+  CONSTRAINT `post_user_fk2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `thread_post_fk2` FOREIGN KEY (`thread_id`) REFERENCES `thread` (`thread_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `post` */
+
+/*Table structure for table `thread` */
+
+DROP TABLE IF EXISTS `thread`;
+
+CREATE TABLE `thread` (
+  `thread_id` int(11) NOT NULL AUTO_INCREMENT,
+  `thread_title` varchar(1000) NOT NULL,
+  `thread_content` blob NOT NULL,
+  `thread_time` time NOT NULL,
+  `thread_date` date NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`thread_id`),
+  KEY `course_fk` (`course_id`),
+  KEY `user_fk` (`user_id`),
+  CONSTRAINT `course_fk` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `thread` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
