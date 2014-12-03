cade6e75e5ae44e7def5a1e9ef4f291c41313b28
lijod <lijod@ccs.neu.edu>
2014-12-03 02:11:11 -0500

    added findAllUsers

--- src/com/lms/service/UserService.java
+++ src/com/lms/service/UserService.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-import com.lms.dao.CourseDao;
 import com.lms.dao.UserCourseDetailDao;
 import com.lms.dao.UserDao;
 import com.lms.model.User;
 import com.lms.model.UserCourseDetail;
-import com.lms.model.UserCourseDetailPK;
 
 // /com.lms.servic/JWSUserService
 @Path("/jwsUserService")
@@ -105,5 +107,14 @@ public class UserService {
 		}
 		return user;
 	}
+	
+	 @POST
+	 @Produces(MediaType.APPLICATION_JSON)
+	 @Consumes(MediaType.APPLICATION_JSON)
+	 @Path("/findAllUsers")
+	 public List<User> findAllUsersService(){
+	  UserDao userDaoObj = new UserDao();
+	  return userDaoObj.findAllUsers();
+	 }
 	
 }
